040d4f3224631ebed322d74b89f6aaa139e57365
Mackenzie James <mackenzie.14@hotmail.co.uk>
2022-02-08 10:33:13 +0000

    security
    only allow dirs to setup/update killfeed settings
    
    relay all !lookups to #lookup

--- commands/lookup.js
+++ commands/lookup.js
@@ -85,67 +85,80 @@ async function getFromSeat (id) {
 module.exports = {
 	id: 'lookup',
 	exec: (call) => {
+        let channellookup = false;
+        let channelID = 0;
         if (call.message.member.roles.cache.find(r => r.name === 'Director')) {
-            const name = call.args.join(' ');
-            let characterID = getCharacterID(name);
-            call.message.channel.send("Searching for " + name + "...");
-            characterID
-            .then(result => getFromSeat(result), stepError)
-            .then(res => {
-                // delete the message the bot sent
+            // check if channel "lookup" exists
+            if (call.message.guild.channels.cache.find(channel => channel.name === 'lookup')) {
+                // get channel id of "lookup"
+                channelID = call.message.guild.channels.cache.find(channel => channel.name === 'lookup').id;
+                channellookup = true
+                console.log(channelID);
+            }
+            if(channellookup) {
+                const name = call.args.join(' ');
+                let characterID = getCharacterID(name);
+                call.message.guild.channels.cache.get(channelID).send(`Searching for ${name} ...\nrequsted by ${call.message.author.username}`);
+                characterID
+                .then(result => getFromSeat(result), stepError)
+                .then(res => {
+                    // delete the message the bot sent
 
-                console.log(res);
-                if(res == null) {
-                    call.message.channel.send('Cant Find Character in Seat');
-                }
-
-                // inside a command, event listener, etc.
-                const embed = new MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle(`${res.name}'s Look Up`)
-                    .setURL(`https://auth.busaesi.space/characters/${res.main_character_id}/sheet`)
-                    .setThumbnail(`https://images.evetech.net/characters/${res.main_character_id}/portrait?size=512`)
-                    .setTimestamp(Date.now())
-                    .setFooter({ text: 'Pulled From SeAT' });
-
-                    // loop through res.associated_character_ids to get all id's
-                    console.log(res.associated_character_ids);
-
-                    const promises = [embed];
-                    for (let i = 0; i < res.associated_character_ids.length; i++) {
-                        promises.push(getCharacterName(res.associated_character_ids[i]));
+                    console.log(res);
+                    if(res == null) {
+                        call.message.guild.channels.cache.get(channelID).send('Cant Find Character in Seat');
                     }
 
-                    return Promise.all(promises);
+                    // inside a command, event listener, etc.
+                    const embed = new MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`${res.name}'s Look Up`)
+                        .setURL(`https://auth.busaesi.space/characters/${res.main_character_id}/sheet`)
+                        .setThumbnail(`https://images.evetech.net/characters/${res.main_character_id}/portrait?size=512`)
+                        .setTimestamp(Date.now())
+                        .setFooter({ text: 'Pulled From SeAT' });
 
-            })
-            .then((arr) => {
+                        // loop through res.associated_character_ids to get all id's
+                        console.log(res.associated_character_ids);
 
-                let embed = arr.shift();
+                        const promises = [embed];
+                        for (let i = 0; i < res.associated_character_ids.length; i++) {
+                            promises.push(getCharacterName(res.associated_character_ids[i]));
+                        }
 
-                var text = "";
-                for(let i = 0; i < arr.length; i++) {
-                    text += arr[i] + '\n';
-                }
+                        return Promise.all(promises);
 
-                embed.addField('Alts', text);
+                })
+                .then((arr) => {
 
-                // check message length
-                if (embed.length > 1024) {
-                    call.message.channel.send(`${res.name}'s Look Up\n${text}`)
-                }
+                    let embed = arr.shift();
+
+                    var text = "";
+                    for(let i = 0; i < arr.length; i++) {
+                        text += arr[i] + '\n';
+                    }
+
+                    embed.addField('Alts', text);
+
+                    // check message length
+                    if (embed.length > 1024) {
+                        call.message.guild.channels.cache.get(channelID).send(`${res.name}'s Look Up\n${text}`)
+                    }
 
 
-                call.message.channel.send({ embeds: [embed] });
+                    call.message.guild.channels.cache.get(channelID).send({ embeds: [embed] });
 
-            })
-            .catch(err => {
-                console.log(err);
-            });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
 
+            } else {
+                call.message.channel.send("Channel lookup does not exist");
+            }
 
         } else {
-            call.message.channel.send('You do not have the required role to do this.\n You must have the role `Director`.');
+            call.message.channel.send('You do not have the required role to do this.\nYou must have the role `Director`.');
             return;
         }
 	}
--- commands/setup-killfeed.js
+++ commands/setup-killfeed.js
@@ -7,69 +7,74 @@ module.exports = {
         let channelID = call.message.channel.id;
 
 
-        // check if config.feeder_channel is greather than 0
-        if (config.feeder_channel == null) {
-            // if not, set it to the current channel
-            
+        // check if users has `Director` role
+        if (call.message.member.roles.cache.find(r => r.name === 'Director')) {
+            // check if config.feeder_channel is greather than 0
+            if (config.feeder_channel == null) {
+                // if not, set it to the current channel
+                
 
 
-            call.prompt('What\'s the lowest value killmails you want to show?\n example `1,000,000,000` would be 1B ISK', { time: 60000 }).then((msg) => {
-                // Resolves with the response.
-                let KillLimit = msg.content;
-                call.prompt('What\'s the lowest value lossmails you want to show?\n example `500,000,000` would be 1B ISK', { time: 60000 }).then((msg) => {
+                call.prompt('What\'s the lowest value killmails you want to show?\n example `1,000,000,000` would be 1B ISK', { time: 60000 }).then((msg) => {
+                    // Resolves with the response.
+                    let KillLimit = msg.content;
+                    call.prompt('What\'s the lowest value lossmails you want to show?\n example `500,000,000` would be 1B ISK', { time: 60000 }).then((msg) => {
 
-                    let LossLimit = msg.content;
-                    call.prompt('Do you want to show pods?\n its enabed by default!, reply with yes or no', { time: 60000 }).then((msg) => {
-                    
-                        let Pod = msg.content;
-
-                        // case switch pod to true to false from yes or no
-                        if (Pod == "yes") {
-                            config.show_pods = true;
-                        } else {
-                            config.show_pods = false;
-                        }
-
-                        // remove any commas from the numbers
-                        KillLimit = KillLimit.replace(/,/g, '');
-                        LossLimit = LossLimit.replace(/,/g, '');
-
-                        config.kill_limit = KillLimit;
-                        config.loss_limit = LossLimit;
+                        let LossLimit = msg.content;
+                        call.prompt('Do you want to show pods?\n its enabed by default!, reply with yes or no', { time: 60000 }).then((msg) => {
                         
-                        config.feeder_channel = channelID;
-                        // edit config.json to update the feeder_channel, kill_limit, and loss_limit
-                        fs.writeFile("./config.json", JSON.stringify(config, null, 4), err => {
-                            if (err) {
-                                console.error(err);
-                                return;
+                            let Pod = msg.content;
+
+                            // case switch pod to true to false from yes or no
+                            if (Pod == "yes") {
+                                config.show_pods = true;
+                            } else {
+                                config.show_pods = false;
                             }
-                            console.log("Config file has been updated");
+
+                            // remove any commas from the numbers
+                            KillLimit = KillLimit.replace(/,/g, '');
+                            LossLimit = LossLimit.replace(/,/g, '');
+
+                            config.kill_limit = KillLimit;
+                            config.loss_limit = LossLimit;
+                            
+                            config.feeder_channel = channelID;
+                            // edit config.json to update the feeder_channel, kill_limit, and loss_limit
+                            fs.writeFile("./config.json", JSON.stringify(config, null, 4), err => {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+                                console.log("Config file has been updated");
+                            });
+
+                            call.message.channel.send('Killfeed setup complete.');
+                            return;
+                        }).catch((exc) => {
+                            // Rejects when the command is cancelled, out of time, or surpasses the maximum amount of attempts.
+                            // In this case surpassing the maximum amount of attempts is impossible since there is no filter.
+                            call.message.channel.send('Cancelled prompt.');
                         });
 
-                        call.message.channel.send('Killfeed setup complete.');
-                        return;
                     }).catch((exc) => {
                         // Rejects when the command is cancelled, out of time, or surpasses the maximum amount of attempts.
                         // In this case surpassing the maximum amount of attempts is impossible since there is no filter.
                         call.message.channel.send('Cancelled prompt.');
                     });
-
                 }).catch((exc) => {
                     // Rejects when the command is cancelled, out of time, or surpasses the maximum amount of attempts.
                     // In this case surpassing the maximum amount of attempts is impossible since there is no filter.
                     call.message.channel.send('Cancelled prompt.');
                 });
-            }).catch((exc) => {
-                // Rejects when the command is cancelled, out of time, or surpasses the maximum amount of attempts.
-                // In this case surpassing the maximum amount of attempts is impossible since there is no filter.
-                call.message.channel.send('Cancelled prompt.');
-            });
+            } else {
+                call.message.channel.send('Killfeed is already setup.');
+            }
+
         } else {
-            call.message.channel.send('Killfeed is already setup.');
+            call.message.channel.send('You do not have the required role to do this.\nYou must have the role `Director`.');
+            return;
         }
-
-
 
     }
 };
